8d40d77954bd2f244d0bbafb8dff4e1dd7273650
SamisNF <sanefre@gmail.com>
2021-08-12 10:23:04 -0500

    End of Day Push
    Made lots of changes... want to save them.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,37 @@
-//Call Hello World
-function helloWorld() {
-    alert("Hello World")
+// get the values from the two <input> tags (controller function)
+function getValues( ) {
+    // get values from the <input> elements
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    alert("The Start Value:" + startValue);
+
+    // Parse the <input> entries from strings to integer values.
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    // call the function generateNumbers
+    let numbers = generateNumbers(startValue, endValue);
+
+    // call the function displayNumbers
+}
+
+// generate the numbers from the startValue to the endValue (logic function)
+function generateNumbers(sValue, eValue) {
+    let numbers = [];
+
+    //we want to generate all numbers from startValue to endValue
+    //loop that takes the sValue and increments it up step-by-step to the eValue.
+    for(let index = sValue; index <= eValue; index++){
+
+        //this will execute in a loop until index is greater than eValue.
+        numbers.push(index);
+    }
+
+    return numbers;
+}
+
+// display the numbers and mark the even numbers bold (display function)
+function displayNumbers( ) {
+
 }
